be946b5373117c64c63c96badeea0837b8ac5895
Pavel <kerus2014@gmail.com>
2023-04-03 16:56:16 +0300

    fix:clean Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,22 +2,14 @@ import styles from "./index.module.scss";
 import { Title } from "../../components/Title";
 import { TitleWithBottomLine } from "../../components/TitleWithBottomLine";
 import { Link } from "../../components/Link";
-import { LinkWithArrow} from "../../components/LinkWithArrow";
+import { LinkWithArrow } from "../../components/LinkWithArrow";
 import { Button } from "../../components/Button";
 import { ListRow } from "../../components/ListRow";
 
 export const Home = () => {
-  return(
+  return (
     <>
-      <Title title="Обо мне"/>
-      <TitleWithBottomLine/>
-      <Link value="Подробнее"/>
-      <LinkWithArrow value="Подробнее"/>
-      <Button value="Click"/>
-      <ListRow value1="2009" value2="Предприниматель
-Собственный бизнес с 2009 года" value3="Летом 2009 отправился в свободное плавание с нулевым бюджетом в кармане. Начал создавать себя и свое дело. Дело, которое сейчас кормит мою семью и помогает двигаться дальше."/>
-      <ListRow value1="2020" value2="Предприниматель
-Собственный бизнес с 2009 года" value3="Летом 2009 отправился в свободное плавание с нулевым бюджетом в кармане. Начал создавать себя и свое дело. Дело, которое сейчас кормит мою семью и помогает двигаться дальше."/>
+      
     </>
-  )
-}
+  );
+};
