fcaef0663abd25a92a464002ad2fb26ba33bcdaf
Pavel <kerus2014@gmail.com>
2023-04-09 17:37:47 +0300

    fix:burger menu

---
+++ src/pages/Services/ProjectSupport/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.scss";
+
+export const ProjectSupport = () => {
+  return (
+    <div>
+      ProjectSupport
+    </div>
+  );
+};
---
+++ src/pages/Services/index.tsx
@@ -0,0 +1,37 @@
+import { InnerNavData } from "../../types";
+import { BlockWithNav } from "../../components/BlockWithNav";
+
+export const Services = () => {
+
+  const navbarData: InnerNavData[] = [
+    {
+      id: 1,
+      value: "Аудит",
+      to:"/services/audit"
+    },
+    {
+      id: 2,
+      value: "Консультация",
+      to:"/services/consulting"
+    },
+    {
+      id: 3,
+      value: "Стратегия под ключ",
+      to:"/services/strategy"
+    },
+    {
+      id: 4,
+      value: "Организация",
+      to:"/services/organization"
+    },
+    {
+      id: 5,
+      value: "Проектная поддержка",
+      to:"/services/project_support"
+    },
+  ];
+
+  return (
+    <BlockWithNav title="Услуги" navData={navbarData}/>
+  );
+};
