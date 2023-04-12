82c16fa77715a3c8e6ec781c90a2589dba488382
mikalaiMikhailavich <wilshere10000@gmail.com>
2023-04-12 19:19:16 +0300

    feat: add adaptive project, about me, cases pages

--- src/components/CaseCard/index.tsx
+++ src/components/CaseCard/index.tsx
@@ -3,19 +3,25 @@ import styles from "./index.module.scss";
 import { ICaseCard } from "../../types";
 import { useNavigate } from "react-router-dom";
 
-export  const CaseCard = (props:ICaseCard) => {
-  const navigate = useNavigate()
-  const {image,title,description,navigatePath} = props
-  return(
+export const CaseCard = (props: ICaseCard) => {
+  const navigate = useNavigate();
+  const { image, title, description, navigatePath } = props;
+  return (
     <div className={styles["case-card"]}>
       <div className={styles["case-card__image"]}>
         <img src={image} alt="" />
       </div>
       <div className={styles["case-card__right-side"]}>
         <p className={styles["case-card__title"]}>{title}</p>
+        <div className={styles["case-card__image_small"]}>
+          <img src={image} alt="" />
+        </div>
         <p className={styles["case-card__description"]}>{description}</p>
-        <LinkWithArrow value="Подробнее" onClick={() => navigate(navigatePath)}/>
+        <LinkWithArrow
+          value="Подробнее"
+          onClick={() => navigate(navigatePath)}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
